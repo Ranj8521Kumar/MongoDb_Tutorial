f2ee0a56bec1bde1b2246d544f955710adb2052b
Ranj8521Kumar <22it3037@rgipt.ac.in>
2024-10-08 16:47:40 +0530

    connect mongodb and insert a document

---
+++ Connect_mongo_and_insertDoc/script.js
@@ -0,0 +1,46 @@
+// Import the MongoDB Node.js driver
+const { MongoClient } = require('mongodb');
+
+// MongoDB connection URI
+const uri = 'mongodb://localhost:27017/';
+
+async function main() {
+    // Connect to MongoDB
+    const client = new MongoClient(uri);
+    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+
+        // Access/Create the "Practice" database
+        const database = client.db('Practice'); // Connecting to the Practice database
+
+        // Access/create the "users" collection
+        const collection = database.collection('users'); // Collection will be created if not existing
+
+        // Insert a new document
+        const result = await collection.insertOne({
+            name: 'Ranjan',
+            age: 21,
+            address: {
+                street: '123 Main St',
+                city: 'Amethi',
+                country: 'India'
+            },
+            hobbies: ['reading', 'traveling', 'photography']
+        });
+
+        // Retrieving a document
+        const document = await collection.findOne({name: "Ranjan Kumar Pandit"});
+        console.log(document);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);  //Error Handling for async Functions
+
